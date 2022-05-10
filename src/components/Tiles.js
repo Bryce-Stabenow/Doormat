e3320303e526b430f0811b2de8ac3ac924c4398f
Bryce Stabenow <brycestabenow617@gmail.com>
2022-05-09 18:01:55 -0700

    updated tile function for favicon automation

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+
 function Tiles({ tiles }) {
-	function directUser() {
-		console.log('redirect');
+	async function getLogo(domain) {
+		let trimDom = domain.replace(/http:\/\//, '');
+		const data = await fetch(`http://favicongrabber.com/api/grab/${trimDom}`);
+		const logoJSON = await data.json();
+
+		setLogoData(logoJSON.icons[0].src);
+		return;
 	}
+
+	const [logoData, setLogoData] = useState('');
+
 	return (
 		<>
 			{tiles.map((tile) => (
-				<div className="singleTile" key={tile.tileNum} onClick={directUser}>
-					{tile.text}
+				<div
+					className="singleTile"
+					key={tile.tileNum}
+					onClick={(e) =>
+						window.open(`${tile.link}`, '_blank', 'noopener', 'noreferrer')
+					}
+				>
+					<h3>{tile.text}</h3>
+					<img
+						className={`tilePhoto ${getLogo(tile.link)}`}
+						src={logoData}
+						alt="Loading..."
+					></img>
 				</div>
 			))}
 		</>
